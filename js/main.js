534fd6689cfea7b490df93f0a2117a6da894df01
zee1013 <dmswl1251@gmail.com>
2026-04-07 13:13:22 +0900

    변수 할당 방법, 변수 연산식

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// window.addEventListener("load" , function(){
+//     실행 코드
+// })
+// window.onload = function(){실행코드}
+// 변수에 요소를 담는다
+        const heading = document.querySelector("#heading")
+        heading.onclick = ()=>{
+            heading.style.color = "red"
+        }
