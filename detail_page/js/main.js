f06f07049183a98cde3b4e2d00eadf9e1d4108cb
zee1013 <dmswl1251@gmail.com>
2026-05-07 15:52:17 +0900

    디테일 페이지 연습

---
+++ detail_page/js/main.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", ()=>{
+    // 요소 선택
+    const cup = document.getElementById("cup")
+    const smallPic = document.querySelectorAll(".small")
+    const viewBtn = document.getElementById("view")
+    const detail = document.getElementById("detail")
+    
+    // 바뀔 정보 내용
+    const descName = document.getElementById("desc-name")
+    const descPrice = document.getElementById("desc-price")
+    const descDelivery = document.getElementById("desc-delivery")
+    const descPoints = document.getElementById("desc-points")
+    const descRoasting = document.getElementById("desc-roasting")
+    // 상세내용 바뀔 정보
+    const detailOrigin = document.getElementById("detail-origin");
+      const detailRegion = document.getElementById("detail-region");
+      const detailFarm = document.getElementById("detail-farm");
+      const detailAltitude = document.getElementById("detail-altitude");
+      const detailVariety = document.getElementById("detail-variety");
+      const detailProcess = document.getElementById("detail-process");
+      const detailDescription = document.getElementById("detail-description");
+      const flavorNote = document.getElementById("flavor-note");
+
+    // 작은 이미지 클릭시 정보 변경(클래스 selected 토글 포함)
+    smallPic.forEach((small)=>{
+        small.addEventListener("click", ()=>{
+            smallPic.forEach((s)=>s.classList.remove("selected"))
+            small.classList.add("selected")
+            changePic(small)
+        })
+    })
+
+    // changePic() : 큰 이미지 변경, 정보 변경
+    function changePic(el){
+        console.log(el); // el : 클릭하는 작은 이미지
+        // 이미지 변경
+        cup.src = el.src
+        // 정보 변경
+        descName.textContent = `상품명 : ${el.dataset.name}`
+        descPrice.textContent = `판매가 : ${el.dataset.price}`
+        descDelivery.textContent = `배송비 : ${el.dataset.delivery}`
+        descPoints.textContent = `적립금 : ${el.dataset.points}`
+        descRoasting.textContent = `로스팅 : ${el.dataset.roasting}`
+
+        // 상세내용 바꾸기
+         detailOrigin.textContent = `원산지 : ${el.dataset.origin}`
+         detailRegion.textContent = `지역 : ${el.dataset.region}`
+         detailFarm.textContent = `농장 : ${el.dataset.farm}`
+         detailAltitude.textContent = `고도 : ${el.dataset.altitude}`
+         detailVariety.textContent = `품종 : ${el.dataset.variety}`
+         detailProcess.textContent = `가공법 : ${el.dataset.process}`
+         detailDescription.textContent = `${el.dataset.description}`
+         flavorNote.textContent = `${el.dataset.flavornote}`
+    }
+
+    // 상세보기 클릭시
+    viewBtn.addEventListener("click", ()=>{
+        detail.classList.toggle("show")
+        // 삼항연산자(if(){}else{}대신 사용)
+        // detail.style.display = detail.style.display === "block" ? "none" : "block"
+    })
+
+})
